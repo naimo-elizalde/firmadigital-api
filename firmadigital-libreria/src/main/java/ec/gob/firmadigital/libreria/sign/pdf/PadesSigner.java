a2718954c3310d120764f2fa87f4f144565faf07
lxgonzalez <luisgx15@gmail.com>
2026-04-14 11:41:20 -0500

    feat: Refactor getSigners method for improved error handling and streamlined PDF processing

--- firmadigital-libreria/src/main/java/ec/gob/firmadigital/libreria/sign/pdf/PadesSigner.java
+++ firmadigital-libreria/src/main/java/ec/gob/firmadigital/libreria/sign/pdf/PadesSigner.java
@@ -43,44 +43,41 @@ public class PadesSigner implements Signer {
 
     @Override
     public List<SignInfo> getSigners(byte[] sign) throws InvalidFormatException, IOException {
-        PdfReader pdfReader;
-        try {
-            try (InputStream is = new ByteArrayInputStream(sign);) {
-                pdfReader = new PdfReader(is);
+        try (InputStream is = new ByteArrayInputStream(sign);
+             PdfReader pdfReader = new PdfReader(is) {
+                 @Override
+                 public boolean hasRebuiltXref() {
+                     return false;
+                 }
+             };
+             PdfDocument pdfDocument = new PdfDocument(pdfReader)) {
+
+            SignatureUtil signatureUtil = new com.itextpdf.signatures.SignatureUtil(pdfDocument);
+
+            @SuppressWarnings("unchecked")
+            List<String> names = signatureUtil.getSignatureNames();
+            List<SignInfo> signInfos = new ArrayList<>();
+            for (String signatureName : names) {
+                com.itextpdf.signatures.PdfPKCS7 pdfPKCS7;
+                try {
+                    pdfPKCS7 = signatureUtil.readSignatureData(signatureName);
+                } catch (Exception e) {
+                    e.printStackTrace();
+                    LOGGER.log(Level.SEVERE, "El PDF contiene una firma corrupta o con un formato desconocido ({0}), se continua con las siguientes si las hubiese: {1}", new Object[]{signatureName, e});
+                    continue;
+                }
+                Certificate[] signCertificateChain = pdfPKCS7.getSignCertificateChain();
+                X509Certificate[] certChain = new X509Certificate[signCertificateChain.length];
+                for (int i = 0; i < certChain.length; i++) {
+                    certChain[i] = (X509Certificate) signCertificateChain[i];
+                }
+                SignInfo signInfo = new SignInfo(certChain, pdfPKCS7.getSignDate().getTime());
+                signInfos.add(signInfo);
             }
+            return signInfos;
         } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "No se ha podido leer el PDF: {0}", e);
-            throw new InvalidFormatException("No se ha podido leer el PDF", e);
-        }
-        SignatureUtil signatureUtil;
-        try {
-            PdfDocument pdfDocument = new PdfDocument(pdfReader);
-            signatureUtil = new com.itextpdf.signatures.SignatureUtil(pdfDocument);
-        } catch (Exception e) {
-            LOGGER.log(Level.SEVERE, "No se ha podido obtener la informacion de los firmantes del PDF, se devolvera un arbol vacio: {0}", e);
+            LOGGER.log(Level.SEVERE, "No se ha podido obtener la informacion de los firmantes del PDF: {0}", e);
             throw new InvalidFormatException("No se ha podido obtener la informacion de los firmantes del PDF", e);
         }
-
-        @SuppressWarnings("unchecked")
-        List<String> names = signatureUtil.getSignatureNames();
-        List<SignInfo> signInfos = new ArrayList<>();
-        for (String signatureName : names) {
-            com.itextpdf.signatures.PdfPKCS7 pdfPKCS7;
-            try {
-                pdfPKCS7 = signatureUtil.readSignatureData(signatureName);
-            } catch (Exception e) {
-                e.printStackTrace();
-                LOGGER.log(Level.SEVERE, "El PDF contiene una firma corrupta o con un formato desconocido ({0}), se continua con las siguientes si las hubiese: {1}", new Object[]{signatureName, e});
-                continue;
-            }
-            Certificate[] signCertificateChain = pdfPKCS7.getSignCertificateChain();
-            X509Certificate[] certChain = new X509Certificate[signCertificateChain.length];
-            for (int i = 0; i < certChain.length; i++) {
-                certChain[i] = (X509Certificate) signCertificateChain[i];
-            }
-            SignInfo signInfo = new SignInfo(certChain, pdfPKCS7.getSignDate().getTime());
-            signInfos.add(signInfo);
-        }
-        return signInfos;
     }
 }
